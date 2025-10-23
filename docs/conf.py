5653d901059cd08aa56a50fcfa3ae9567e62b24f
Hector E Mozo <hector.mozo@neureonmindfluxlab.org>
2025-10-23 16:56:07 -0400

    Update author and source path in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,10 +2,10 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../src"))
 
 project = "qmlhc Minimal Core Demo"
-author = "Héctor Mozo"
+author = "Neureonmindflux Research Lab"
 release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
