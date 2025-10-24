4499fbe535ffd2fba86d0b9aac154eee3a228ae0
Hector E Mozo <hector.mozo@neureonmindfluxlab.org>
2025-10-24 11:33:52 -0400

    Change HTML theme from sphinx_rtd_theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 language = "en"
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
 
 # -- Napoleon settings -------------------------------------------------------
